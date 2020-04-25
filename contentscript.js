40618ce3cbde8ab6b5f6f56d917b41c9d4614342
Taichi Kato <taichikato0909@gmail.com>
2020-04-25 23:27:17 +0800

    Update contentscript.js

--- contentscript.js
+++ contentscript.js
@@ -1,36 +1,3 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-function buildLinkDisplayContainer() {
-  var linkDisplayContainer = document.createElement('div');
-  linkDisplayContainer.setAttribute('id', 'link-display-div');
-  linkDisplayContainer.setAttribute('class', 'link-display');
-
-  return linkDisplayContainer;
-}
-
-function showLinkDestination(e) {
-  var linkHrefText = document.createTextNode(e.target.href),
-      linkDisplayContainer = buildLinkDisplayContainer();
-
-  linkDisplayContainer.appendChild(linkHrefText);
-  document.body.appendChild(linkDisplayContainer);
-}
-
-function hideLinkDestination(e) {
-  var container = document.getElementById('link-display-div');
-  container.parentNode.removeChild(container);
-}
-
-function moveLinkDisplay(e) {
-  var container = document.getElementById('link-display-div');
-
-  container.style.position = 'absolute';
-  container.style.left = (e.pageX + 15) + 'px';
-  container.style.top = (e.pageY + 15) + 'px';
-}
-
 function initListeners() {
   var anchors = $('a[href^="mailto:"]')
   console.log(anchors)
